13f82b5f5d40148e197243fd04138d2b5aabf5bc
Babonea Ionuț Alexandru <babonea.ionut.alexandru@gmail.com>
2026-06-02 17:32:52 +0300

    replaced time.sleep() with threading.Event().wait() for immediate stopping

--- autoclicker.py
+++ autoclicker.py
@@ -1,28 +1,35 @@
 from time import sleep
-from threading import Thread
+from threading import Thread,Event
 from pynput.keyboard import Listener,Key
 from pynput.mouse import Controller,Button
 
 mouse=Controller()
 mouseButton=Button.left
 
-delay=1
+delay=5
 clickerActive=False
 start_stop_key=Key.f4
 exit_key=Key.esc
+
+stop_event=Event()
 
 def checkPressedKey(pressedKey):
     global clickerActive
     if pressedKey==start_stop_key:
         clickerActive=not clickerActive
+        if not clickerActive:
+            stop_event.set()
     elif pressedKey==exit_key:
+        clickerActive=False
+        stop_event.set()
         return False
     
 def autoClicker():
     while True:
         if clickerActive:
             mouse.click(mouseButton)
-            sleep(delay)
+            stop_event.clear()
+            stop_event.wait(delay)
         else:
             sleep(0.1)
 
@@ -30,3 +37,7 @@ Thread(target=autoClicker,daemon=True).start()
 
 with Listener(on_press=checkPressedKey) as listener:
     listener.join()
+
+
+
+
